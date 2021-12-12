4ad510f0e2f8c31742e859b40cabad95947c4511
mensur056 <mansur.sarxanov@gmail.com>
2021-12-12 23:16:20 +0400

    test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
 
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
