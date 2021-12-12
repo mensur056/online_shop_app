64a3791bbca5317a7d5585ce3b2acdb9bc6458e7
mensur056 <mansur.sarxanov@gmail.com>
2021-12-12 23:15:28 +0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_shop_app/constants.dart';
 import 'package:online_shop_app/screens/home_screen.dart';
 
 void main() {
@@ -9,7 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+        debugShowCheckedModeBanner: false,
+        title: "Flutter de",
+        theme: ThemeData(
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme:
+                Theme.of(context).textTheme.apply(bodyColor: kTextColor)),
         home: HomeScreen());
   }
 }
