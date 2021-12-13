30dc094eed2fb5ca8c48b1b9bac908c0622fc656
mensur056 <mansur.sarxanov@gmail.com>
2021-12-13 11:53:44 +0400

    test

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -28,9 +28,15 @@ class Categories extends StatefulWidget {
 
 class _CategoriesState extends State<Categories> {
   List<String> categories = ['Hand bag', 'Jewellery', 'Footwear', 'Dresses'];
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) => Text(categories[index])),
+    );
   }
 }
