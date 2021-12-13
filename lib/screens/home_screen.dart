c7ce7c187f3c5c7099714bb5abf47433255bddb2
mensur056 <mansur.sarxanov@gmail.com>
2021-12-13 11:43:34 +0400

    test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,20 +6,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset('assets/icons/back (1).svg'),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset('assets/icons/search (1).svg',color: kTextColor,),
-          ),
-        ],
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset('assets/icons/back (1).svg'),
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/search (1).svg',color: kTextColor,),
+        ), IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset('assets/icons/cart.svg',color: kTextColor,),
+        ),SizedBox(width: kDefaultPadding/2,)
+      ],
     );
   }
 }
