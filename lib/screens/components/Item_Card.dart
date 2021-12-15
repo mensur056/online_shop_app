853a8240f74bb3a65951e7dcb4be41631b21b675
mensur056 <mansur.sarxanov@gmail.com>
2021-12-15 19:27:25 +0400

    test

--- lib/screens/components/Item_Card.dart
+++ lib/screens/components/Item_Card.dart
@@ -5,7 +5,7 @@ import '../../constants.dart';
 
 class ItemCard extends StatelessWidget {
   final Product product;
-  final Function press;
+  final VoidCallback press;
 
   const ItemCard({
     required this.product,
@@ -14,28 +14,30 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-                color: product.color, borderRadius: BorderRadius.circular(16)),
-            child: Image.asset(product.image,),
+    return GestureDetector(onTap: press,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: product.color, borderRadius: BorderRadius.circular(16)),
+              child: Image.asset(product.image,),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: kDefaultPadding / 4),
-          child: Text(
-            product.title,
-            style: TextStyle(color: kTextLightColor),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: kDefaultPadding / 4),
+            child: Text(
+              product.title,
+              style: TextStyle(color: kTextLightColor),
+            ),
           ),
-        ),
-        Text(
-          '\$${product.price}',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        )
-      ],
+          Text(
+            '\$${product.price}',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 }
