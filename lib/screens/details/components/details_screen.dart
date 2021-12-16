1780b80cc2e7599887280710a7ed733f3afe71c9
mensur056 <mansur.sarxanov@gmail.com>
2021-12-16 21:30:08 +0400

    test

--- lib/screens/details/components/details_screen.dart
+++ lib/screens/details/components/details_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:online_shop_app/models/products.dart';
+
 class DetailsScreen extends StatelessWidget {
-final Product product;
+  final Product product;
 
   const DetailsScreen({Key? key, required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: product.color,);
+    return Scaffold(
+        backgroundColor: product.color,
+        appBar: AppBar(
+          backgroundColor: product.color,
+          leading: IconButton(
+            icon: SvgPicture.asset('assets/icons/back (1).svg'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ));
   }
 }
