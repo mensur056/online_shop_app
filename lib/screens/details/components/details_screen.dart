c4b299699b332fc1971441d529af1992a300ca87
mensur056 <mansur.sarxanov@gmail.com>
2021-12-17 09:33:02 +0400

    test

--- lib/screens/details/components/details_screen.dart
+++ lib/screens/details/components/details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_shop_app/constants.dart';
 import 'package:online_shop_app/models/products.dart';
+import 'package:online_shop_app/screens/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
@@ -11,34 +13,38 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: product.color,
-      appBar: AppBar(
-        backgroundColor: product.color,
-        elevation: 0,
-        leading: IconButton(
+      appBar: buildAppBar(context),body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          'assets/icons/back (1).svg',
+          color: Colors.white,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
           icon: SvgPicture.asset(
-            'assets/icons/back (1).svg',
+            'assets/icons/search (1).svg',
             color: Colors.white,
           ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              'assets/icons/search (1).svg',
-              color: Colors.white,
-            ),
-          ), IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              'assets/icons/cart.svg',
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
+        ), IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            'assets/icons/cart.svg',
+            color: Colors.white,
+          ),
+        ),SizedBox(width: kDefaultPadding/2,)
+      ],
     );
   }
 }
