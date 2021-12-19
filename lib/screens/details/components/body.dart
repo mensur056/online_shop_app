ba65f84268654cd5da74097d911e8630912ad268
mensur056 <mansur.sarxanov@gmail.com>
2021-12-19 09:06:27 +0400

    test

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:online_shop_app/constants.dart';
 import 'package:online_shop_app/models/products.dart';
+import 'package:online_shop_app/screens/details/components/product_title_with_image.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -35,67 +36,6 @@ class Body extends StatelessWidget {
                 ProductTitleWithImage(product: product)
               ],
             ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class ProductTitleWithImage extends StatelessWidget {
-  const ProductTitleWithImage({
-
-    required this.product,
-  }) ;
-
-  final Product product;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            product.model,
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            product.title,
-            style: Theme.of(context).textTheme.headline4!.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 35.0),
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                          text: 'Price\n',
-                          style: TextStyle(color: Colors.white)),
-                      TextSpan(
-                          text: "\$${product.price}",
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline4!
-                              .copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold))
-                    ],
-                  ),
-                ),
-              ),
-              Spacer(),
-              Expanded(
-                child: Image.asset(
-                  product.image,fit: BoxFit.fill,
-                ),
-              )
-            ],
           )
         ],
       ),
---
+++ lib/screens/details/components/product_title_with_image.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop_app/models/products.dart';
+
+import '../../../constants.dart';
+
+class ProductTitleWithImage extends StatelessWidget {
+  const ProductTitleWithImage({
+
+    required this.product,
+  }) ;
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            product.model,
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            product.title,
+            style: Theme.of(context).textTheme.headline4!.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 35.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: 'Price\n',
+                          style: TextStyle(color: Colors.white)),
+                      TextSpan(
+                          text: "\$${product.price}",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4!
+                              .copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              Expanded(
+                child: Image.asset(
+                  product.image,fit: BoxFit.fill,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
