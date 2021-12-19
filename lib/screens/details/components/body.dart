2080852480a2d3ef6abbaabe1cf7fc5793636be9
mensur056 <mansur.sarxanov@gmail.com>
2021-12-19 09:04:18 +0400

    test

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -24,64 +24,78 @@ class Body extends StatelessWidget {
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(
-                        12,
+                        24,
                       ),
                       topRight: Radius.circular(
-                        12,
+                        24,
                       ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        product.model,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        product.title,
-                        style: Theme.of(context).textTheme.headline4!.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 35.0),
-                            child: RichText(
-                              text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                      text: 'Price\n',
-                                      style: TextStyle(color: Colors.white)),
-                                  TextSpan(
-                                      text: "\$${product.price}",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline4!
-                                          .copyWith(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold))
-                                ],
-                              ),
-                            ),
-                          ),
-                          Spacer(),
-                          Expanded(
-                            child: Image.asset(
-                              product.image,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                ProductTitleWithImage(product: product)
               ],
             ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductTitleWithImage extends StatelessWidget {
+  const ProductTitleWithImage({
+
+    required this.product,
+  }) ;
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            product.model,
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            product.title,
+            style: Theme.of(context).textTheme.headline4!.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 35.0),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: 'Price\n',
+                          style: TextStyle(color: Colors.white)),
+                      TextSpan(
+                          text: "\$${product.price}",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4!
+                              .copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              Expanded(
+                child: Image.asset(
+                  product.image,fit: BoxFit.fill,
+                ),
+              )
+            ],
           )
         ],
       ),
